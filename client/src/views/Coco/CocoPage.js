5bbe50bc4adf177164393cf54fc596ac00e237e1
ares20070408 <StephaniaWindheimmqE@gmail.com>
2026-03-24 08:16:35 -0700

    Update Coco

--- client/src/views/Coco/CocoPage.js
+++ client/src/views/Coco/CocoPage.js
@@ -554,7 +554,7 @@ export default function CocoPage() {
 
                         setAnimState("idle");
 
-                    }, 200);
+                    }, 100);
     
                 } 
                 else {
